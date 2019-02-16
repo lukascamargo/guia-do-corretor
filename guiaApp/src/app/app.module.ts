0eebb4836ad2362fc5b2cce316725689280dee52
Lukas Camargo (Americas) <lukas.camargo@dimensiondata.com>
2019-02-16 01:29:57 -0200

    UPDATE | Corrigindo erro de rotas

--- guiaApp/src/app/app.module.ts
+++ guiaApp/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TemplateModule } from './template/template.module';
+import { SigninModule } from './signin/signin.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateModule,
+    SigninModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- guiaApp/src/app/template/template.module.ts
+++ guiaApp/src/app/template/template.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComunidadeModule } from './comunidade/comunidade.module';
+import { HomeModule } from './home/home.module';
+import { NoticiasModule } from './noticias/noticias.module';
+import { TrilhaModule } from './trilha/trilha.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ComunidadeModule,
+    HomeModule,
+    NoticiasModule,
+    TrilhaModule
   ]
 })
 export class TemplateModule { }
